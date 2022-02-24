a067d6fe637c7ac948f285c2b16c11e334bcade5
anaclaudiabarreto <80101582+anaclaudiabarreto@users.noreply.github.com>
2022-02-24 14:37:13 -0300

    Add project files.

---
+++ Classes/Serie.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Series
+{
+    public class Serie : EntidadeBase
+    {
+        //Atributos
+        private Genero Genero { get; set; }        
+        private string Titulo { get; set; }
+        private string Sinopse { get; set; }
+        private int Ano { get; set; }
+        private Classificacao Classificacao { get; set; }
+
+        //private int ConteudosSensiveis ConteudosSensiveis { get; set; }
+        private bool Excluido { get; set; }
+
+        //Métodos
+        public Serie(int id, Genero genero, string titulo, string sinopse, int ano, Classificacao classificacao)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Sinopse = sinopse;
+            this.Ano = ano;
+            this.Classificacao = classificacao;
+            this.Excluido = false;
+
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " +this.Genero + Environment.NewLine;
+            retorno += "Título: " +this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " +this.Sinopse + Environment.NewLine;
+            retorno += "Classificação: " +this.Classificacao + Environment.NewLine;
+            retorno += "Ano de Início: " +this.Ano;
+            retorno += "Excluído: " +this.Excluido;
+            return retorno;
+        }
+
+        public string RetornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int RetornaId()
+        {
+            return this.Id;
+        }
+
+        public bool RetornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
---
+++ Enum/Classificacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Series
+{
+    public enum Classificacao
+    {
+        Livre = 0,
+        MaioresDe10Anos = 10,
+        MaioresDe12Anos = 12,
+        MaioresDe14Anos = 14,
+        MaioresDe16Anos = 16,
+        MaioresDe18Anos = 18
+    }
+}
